b066a859f56d32c50aa6340cd7307ad05db6932a
jcristiani <jcristia10@gmail.com>
2022-01-18 18:08:04 -0800

    update grid extent

--- scripts/dst_preprocess/01_grid_coast.py
+++ scripts/dst_preprocess/01_grid_coast.py
@@ -8,6 +8,7 @@ base = r'C:\Users\jcristia\Documents\GIS\DFO\DST_pilot\spatial'
 coastline = os.path.join(base, r'01_original\Pacific_fine_scale_ocean_fromTRIM_20210615.gdb\Pacific_Ocean_fromTRIM_20210609')
 grid_2km = os.path.join(base, r'01_original\DSTpilot_grids.gdb\GRID_MARX_planningUnits2x2_bc')
 gdb = os.path.join(base, r'03_working\dst_grid.gdb')
+ss_bioregion_EXTENT = 'DFO_Marine_Bioregions_SouthernShelf_CUSTOMEXTENT'
 arcpy.env.workspace = gdb
 
 
@@ -25,16 +26,28 @@ arcpy.SubdividePolygon_management(
     subdivision_type='STACKED_BLOCKS'
     )
 
+# clip to Southern Shelf Bioregion
+# HOWEVER, the coastline used for bioregions is not as high resolution as the
+# TRIM coastline. If I clip with just this then some PUs will be removed that
+# should not be. Therefore, I need to get a general extent of the SSBR to clip
+# to.
+# I MANUALLY drew this, by just making a general shape that includes all of the
+# coastline and then traced the pelagic outline of the SSBR.
+arcpy.Clip_analysis('dst_grid1km_01divide', ss_bioregion_EXTENT, 'dst_grid1km_02clip')
+
 
 # remove any grid cells that don't overlap with the coastline
 # (this takes a few minutes to run, but it does work)
-arcpy.MakeFeatureLayer_management('dst_grid1km_01divide', 'temp_lyr')
+arcpy.MakeFeatureLayer_management('dst_grid1km_02clip', 'temp_lyr')
 sel_grid = arcpy.SelectLayerByLocation_management(
     'temp_lyr',
     'INTERSECT',
     coastline,
     selection_type='NEW_SELECTION'
 )
+arcpy.env.outputCoordinateSystem = arcpy.SpatialReference(3005)
 arcpy.CopyFeatures_management(sel_grid, 'dst_grid1km')
+
 arcpy.Delete_management('temp_lyr')
 arcpy.Delete_management('dst_grid1km_01divide')
+arcpy.Delete_management('dst_grid1km_02clip')
