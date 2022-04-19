525b9608d13068260bcb07eac3a6d09cdbcd4700
jcristiani <jcristia10@gmail.com>
2022-04-18 17:50:22 -0700

    remove comments

--- scripts/dst_04_postprocess/01_postprocessing_prioritizr.py
+++ scripts/dst_04_postprocess/01_postprocessing_prioritizr.py
@@ -74,68 +74,6 @@ for dir in dirs:
     arcpy.Delete_management('tbl_TEMP')
 
 
-# COMMENTING THIS OUT FOR NOW. I don't think I need this.
-# I'll keep the code here until I know for sure though.
-# it will also be helpful to have sel_freq, importance, and solution_1 split
-# out into different fcs and have a column for each scenario
-# i = 0
-# for dir in dirs:
-#     solution = os.path.join(root, output_dir, dir, 'solution.csv')
-#     df = pd.read_csv(solution)
-#     sol_num = dir.split('_')[0]
-
-#     # calculate selection frequency
-#     #cols = ['id', 'rc']
-#     cols = ['uID']
-#     sol_cols = [col for col in df.columns if col.startswith('solution')]
-#     cols.extend(sol_cols)
-#     df = df[cols]
-#     df['sel_freq'] = df.loc[:, sol_cols].sum(axis=1) / len(sol_cols)
-
-#     # keep just the top solution
-#     #df = df[['id', 'rc', 'solution_1', 'sel_freq']]
-#     df = df[['uID', 'solution_1', 'sel_freq']]
-#     # make unique column names
-#     df = df.rename(columns={
-#         #'rc':f'{sol_num}_rc',
-#         'solution_1':f'{sol_num}_sol1',
-#         'sel_freq':f'{sol_num}_selfreq'
-#     })
-
-#     # put into individual dataframes
-#     #df_rc = df[['id', f'{sol_num}_rc']]
-#     df_sol1 = df[['uID', f'{sol_num}_sol1']]
-#     df_selfreq = df[['uID', f'{sol_num}_selfreq']]
-
-#     # append to individual dataframes
-#     if i == 0:
-#         #df_rc_all = df_rc
-#         df_sol1_all = df_sol1
-#         df_selfreq_all = df_selfreq
-#     else:
-#         #df_rc_all = df_rc_all.merge(df_rc, on='id')
-#         df_sol1_all = df_sol1_all.merge(df_sol1, on='uID')
-#         df_selfreq_all = df_selfreq_all.merge(df_selfreq, on='uID')
-#     i += 1
-
-# #dfs = [df_rc_all, df_sol1_all, df_selfreq_all]
-# dfs = [df_sol1_all, df_selfreq_all]
-# #names = ['sg_all_rc', 'sg_all_solution1', 'sg_all_selfreq']
-# names = ['sg_all_solution1', 'sg_all_selfreq']
-# for df, name in zip(dfs, names):
-#     # output to gdb table
-#     x = np.array(np.rec.fromrecords(df.values))
-#     names = df.dtypes.index.tolist()
-#     x.dtype.names = tuple(names)
-#     arcpy.da.NumPyArrayToTable(x, os.path.join(arcpy.env.workspace, 'tbl_TEMP'))
-#     # join to copy of point feature class
-#     jointbl = arcpy.AddJoin_management(pus, 'uID', 'tbl_TEMP', 'uID')
-#     arcpy.CopyFeatures_management(jointbl, name)
-#     arcpy.DeleteField_management(name, ['OBJECTID_1', 'id_1'])
-#     arcpy.Delete_management(jointbl)
-#     arcpy.Delete_management('tbl_TEMP')
-
-
 
 ### Irreplaceability ###
 
